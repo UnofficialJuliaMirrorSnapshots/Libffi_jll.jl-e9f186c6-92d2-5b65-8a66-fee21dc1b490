c65803c5212a608614f4eae7c5445e4ba4a16b4a
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-09-08 07:59:06 +0000

    Snapshot of branch master taken on 2019-09-08T03:59:04.906-04:00 from "https://github.com/UnofficialJuliaMirror/Libffi_jll.jl-e9f186c6-92d2-5b65-8a66-fee21dc1b490"

---
+++ src/Libffi_jll.jl
@@ -0,0 +1,26 @@
+module Libffi_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("e9f186c6-92d2-5b65-8a66-fee21dc1b490"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "Libffi", artifacts_toml) for e in artifacts["Libffi"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load Libffi; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module Libffi_jll
